4c9b5d49b293079d22ab4f56bebc8bd735f68ebb
basarat <basaratali@gmail.com>
2024-07-24 18:12:06 +1000

    wip

---
+++ tests/000/30.autoretry.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+// 📝 skipped as it is designed to fail
+test.skip("Page demo", async ({ page }) => {
+  // arrange
+  await page.goto("https://google.com");
+
+  // act
+  const input = page.locator("[title=Searchs]"); // 👀 typo
+  await input.fill("playwright"); // 👀 note that the wait (retries) happens here
+  await input.press("Enter");
+
+  // assert
+  await expect(page).toHaveTitle("playwright - Google Search");
+});
