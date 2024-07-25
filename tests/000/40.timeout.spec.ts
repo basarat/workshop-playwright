9f8b4860d4e52f8c2302649e364d390bd2111558
basarat <basaratali@gmail.com>
2024-07-25 11:42:36 +1000

    done

--- tests/000/40.timeout.spec.ts
+++ tests/000/40.timeout.spec.ts
@@ -2,15 +2,15 @@ import { test, expect } from "@playwright/test";
 
 // 📝 skipped as it is designed to fail
 test.skip("Page demo", async ({ page }) => {
-  /** Can also be set globally using config `timeout` option (ms) */
-  test.setTimeout(2 * 1000); // 👀 set timeout to 2 seconds
+  /** 👀 Can also be set globally using config `timeout` option (ms) */
+  test.setTimeout(10 * 1000); // 👀 set timeout to 10 seconds
 
   // arrange
   await page.goto("https://google.com");
 
   // act
   const input = page.locator("[title=Searchs]"); // 👀 typo
-  await input.fill("playwright"); // 👀 note that the wait (retries) happens here
+  await input.fill("playwright", { timeout: 2 * 1000 }); // 👀 custom timeout for individual actions
   await input.press("Enter");
 
   // assert
