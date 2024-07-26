f8bd7faccbc6b65c54e6410d2f7cb336c94141d5
basarat <basaratali@gmail.com>
2024-07-26 12:45:02 +1000

    get all

---
+++ src/app/api/get-all/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getDb } from "../../../server/db";
+import { GetAllResponse } from "../../../common/types";
+
+export default async function GET(_req: NextRequest) {
+  const response: GetAllResponse = {
+    items: (await getDb()).data.items,
+  };
+  return NextResponse.json(response);
+}
