d1af6c4415ed2728b3e7c358e60dba813c15040a
basarat <basaratali@gmail.com>
2024-07-26 13:52:41 +1000

    not required

--- tests/010/30.expectany.spec.ts
+++ tests/010/30.expectany.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('expect.any', async ({ request }) => {
+test('expect.any', async ({}) => {
   const id = Math.random().toString();
   expect(id).toEqual(expect.any(String));
 
