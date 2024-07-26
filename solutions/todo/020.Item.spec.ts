4f373fc067498ad51fc653ec0e9aec59e35498f6
basarat <basaratali@gmail.com>
2024-07-26 17:56:07 +1000

    another

--- solutions/todo/020.Item.spec.ts
+++ solutions/todo/020.Item.spec.ts
@@ -7,17 +7,19 @@ test.beforeEach(async ({ page, request }) => {
 
 test.describe('Item', () => {
   test('Starts of unchecked', async ({ page }) => {
-    const todoPage = new TodoPage(page)
+    const todoPage = new TodoPage(page);
     await todoPage.addTodo('Hello World');
     await expect(todoPage.itemCheckboxByIndex(0)).not.toBeChecked();
   });
-  // it('Clicking the checkbox toggles the todo active/complete', () => {
-  //   todoPage.addTodo('Hello World');
-  //   todoPage.itemCheckboxByIndex(0).click();
-  //   todoPage.itemCheckboxByIndex(0).should('be.checked');
-  //   todoPage.itemCheckboxByIndex(0).click();
-  //   todoPage.itemCheckboxByIndex(0).should('not.be.checked');
-  // });
+
+  test('Clicking the checkbox toggles the todo active/complete', async ({ page }) => {
+    const todoPage = new TodoPage(page);
+    await todoPage.addTodo('Hello World');
+    await todoPage.itemCheckboxByIndex(0).click();
+    await expect(todoPage.itemCheckboxByIndex(0)).toBeChecked();
+    await todoPage.itemCheckboxByIndex(0).click();
+    await expect(todoPage.itemCheckboxByIndex(0)).not.toBeChecked();
+  });
   // it('Clicking the remove button should remove it item', () => {
   //   todoPage.addTodo('Hello World');
   //   todoPage.itemDestroyByIndex(0).invoke('show').click();
