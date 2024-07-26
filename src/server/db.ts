0f6bd898fd71e5197fb4eb0d4b195b21eb0c1868
basarat <basaratali@gmail.com>
2024-07-26 17:03:22 +1000

    docs

--- src/server/db.ts
+++ src/server/db.ts
@@ -11,14 +11,18 @@ type DBSchema = {
 };
 
 /** Use JSON file for storage */
-const file = path.join(__dirname, '../../db.json');
+const file = path.join(__dirname, 'db.json');
+
+/** Create the json file adapter */
 const adapter = new JSONFile<DBSchema>(file);
 
-/** The DB */
+/** The DB powered by the adapter */
 const db = new Low(adapter, { items: [] });
-
 
 export async function getDb() {
   await db.read();
+  // console.log('db.data', db.data);
+  // db.data.items.push({ id: 'first', completed: false, message: 'testing' });
+  // await db.write();
   return db;
 }
