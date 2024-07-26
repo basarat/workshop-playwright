97817dc97bd2586093d9a709b648816388ab1d6a
basarat <basaratali@gmail.com>
2024-07-26 17:42:02 +1000

    another test

--- solutions/todo/010.NewTodo.spec.ts
+++ solutions/todo/010.NewTodo.spec.ts
@@ -9,20 +9,29 @@ test.describe('New Todo', () => {
   test('The input element should be focused when the page is loaded', async ({ page }) => {
     await expect(new TodoPage(page).newTodoInput).toBeFocused();
   });
+
   test('Created by enter, adding it to the list', async ({ page }) => {
     const todoPage = new TodoPage(page);
     await todoPage.newTodoInput.fill('Hello world');
     await todoPage.newTodoInput.press('Enter');
     await expect(todoPage.itemLabelByIndex(0)).toContainText('Hello world');
   });
-  // it('Clear input after adding', () => {
-  //   todoPage.newTodoInput.type('Hello world').type('{enter}');
-  //   todoPage.newTodoInput.should('have.value', '');
-  // });
-  // it('`.trim` before adding', () => {
-  //   todoPage.newTodoInput.type(' Hello world ').type('{enter}');
-  //   todoPage.itemLabelByIndex(0).should('have.text', 'Hello world');
-  // });
+
+  test('Clear input after adding', async ({ page }) => {
+    const todoPage = new TodoPage(page);
+    await todoPage.newTodoInput.fill('Hello world');
+    await expect(todoPage.newTodoInput).toHaveValue('Hello world');
+    await todoPage.newTodoInput.press('Enter');
+    await expect(todoPage.newTodoInput).toHaveValue('');
+  });
+
+  test('`.trim` before adding', async ({ page }) => {
+    const todoPage = new TodoPage(page);
+    await todoPage.newTodoInput.fill(' Hello world ');
+    await todoPage.newTodoInput.press('Enter');
+    await expect(todoPage.itemLabelByIndex(0)).toHaveText('Hello world');
+  });
+
   // it('Do not create a todo if the result of trim is an empty string', () => {
   //   todoPage.newTodoInput.type('  ').type('{enter}');
   //   todoPage.itemLabelByIndex(0).should('not.exist');
