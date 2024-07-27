e1e46c06e94318e1006df7e85888055b9d0d0531
basarat <basaratali@gmail.com>
2024-07-27 12:36:07 +1000

    better

---
+++ solutions/todo/040.ToggleAll.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+import { TodoPage } from './todoPage';
+
+test.beforeEach(async ({ page, request }) => {
+  await new TodoPage(page).clearTodosAndVisit(request);
+});
+
+test.describe('Toggle All', () => {
+  test('Should not be visible when there are no todos', async ({ page }) => {
+    const todoPage = new TodoPage(page);
+    await expect(todoPage.toggleAllCheckbox).not.toBeAttached();
+    await todoPage.addTodo('Hello');
+    await expect(todoPage.toggleAllCheckbox).toBeAttached();
+  });
+  // it('If any todo is not complete it should not be checked.', () => {
+  //   todoPage.addTodo('Hello');
+  //   todoPage.addTodo('World');
+  //   /** When nothing is checked */
+  //   todoPage.toggleAllCheckbox.should('not.be.checked');
+  //   /** When mixed */
+  //   todoPage.itemCheckboxByIndex(0).click();
+  //   todoPage.toggleAllCheckbox.should('not.be.checked');
+  // });
+  // it('When all the todos are checked it should also get checked.', () => {
+  //   todoPage.addTodo('Hello');
+  //   todoPage.addTodo('World');
+  //   todoPage.itemCheckboxByIndex(0).click();
+  //   todoPage.itemCheckboxByIndex(1).click();
+  //   todoPage.toggleAllCheckbox.should('be.checked');
+  // });
+  // it('This checkbox toggles all the todos to the same state as itself', () => {
+  //   todoPage.addTodo('Hello');
+  //   todoPage.addTodo('World');
+
+  //   /** When clicked, If it is not checked, it checks all todos. */
+  //   todoPage.toggleAllCheckbox.click();
+  //   todoPage.itemCheckboxByIndex(0).should('be.checked');
+  //   todoPage.itemCheckboxByIndex(1).should('be.checked');
+  //   todoPage.toggleAllCheckbox.should('be.checked');
+
+  //   /** When clicked, If it is checked, it unchecks all todos. */
+  //   todoPage.toggleAllCheckbox.click();
+  //   todoPage.itemCheckboxByIndex(0).should('not.be.checked');
+  //   todoPage.itemCheckboxByIndex(1).should('not.be.checked');
+  //   todoPage.toggleAllCheckbox.should('not.be.checked');
+  // });
+});
