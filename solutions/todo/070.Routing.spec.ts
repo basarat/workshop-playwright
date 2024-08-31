17d8d36cee3303781a2a0f037d98ed9357a862fb
basarat <basaratali@gmail.com>
2024-08-31 20:19:52 +1000

    better

--- solutions/todo/070.Routing.spec.ts
+++ solutions/todo/070.Routing.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '../../tests/todo/test';
-import { TodoPage } from '../../tests/todo/todoPage';
 
 test.beforeEach(async ({ clearTodosAndVisit }) => {
   await clearTodosAndVisit();
@@ -13,32 +12,27 @@ test.describe('Routing', () => {
     await todoPage.addTodo('Active');
   });
 
-  test('"#/" (default) - all items are shown. The all link is selected', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('"#/" (default) - all items are shown. The all link is selected', async ({ todoPage }) => {
     await todoPage.expectTodos(['Completed', 'Active']);
     await expect(todoPage.routeAll).toHaveClass(todoPage.classNames.selectedRoute);
   });
-  test('"#/active" - Only active items are shown. The active link is selected', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('"#/active" - Only active items are shown. The active link is selected', async ({ todoPage }) => {
     await todoPage.goto('active');
     await todoPage.expectTodos(['Active']);
     await expect(todoPage.routeActive).toHaveClass(todoPage.classNames.selectedRoute);
   });
-  test('"#/completed" - Only completed items are shown. The completed link is selected', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('"#/completed" - Only completed items are shown. The completed link is selected', async ({ todoPage }) => {
     await todoPage.goto('completed');
     await todoPage.expectTodos(['Completed']);
     await expect(todoPage.routeCompleted).toHaveClass(todoPage.classNames.selectedRoute);
   });
-  test('"#/active" - Items should move out if checked', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('"#/active" - Items should move out if checked', async ({ todoPage }) => {
     await todoPage.goto('active');
     await todoPage.itemCheckboxByIndex(0).click();
     await expect(todoPage.itemLabelByIndex(0)).not.toBeAttached();
     await todoPage.expectTodos([]);
   });
-  test('"#/completed" - Items should move out if unchecked', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('"#/completed" - Items should move out if unchecked', async ({ todoPage }) => {
     await todoPage.goto('completed');
     await todoPage.itemCheckboxByIndex(0).click();
     await expect(todoPage.itemLabelByIndex(0)).not.toBeAttached();
