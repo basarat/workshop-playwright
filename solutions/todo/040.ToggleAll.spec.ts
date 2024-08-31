3f5ee36a43ff5c6e40633eaf85d55816f9de58d0
basarat <basaratali@gmail.com>
2024-08-31 20:21:42 +1000

    better

--- solutions/todo/040.ToggleAll.spec.ts
+++ solutions/todo/040.ToggleAll.spec.ts
@@ -1,19 +1,16 @@
 import { test, expect } from '../../tests/todo/test';
-import { TodoPage } from '../../tests/todo/todoPage';
 
 test.beforeEach(async ({ clearTodosAndVisit }) => {
   await clearTodosAndVisit();
 });
 
 test.describe('Toggle All', () => {
-  test('Should not be visible when there are no todos', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('Should not be visible when there are no todos', async ({ todoPage }) => {
     await expect(todoPage.toggleAllCheckbox).not.toBeAttached();
     await todoPage.addTodo('Hello');
     await expect(todoPage.toggleAllCheckbox).toBeAttached();
   });
-  test('If any todo is not complete it should not be checked.', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('If any todo is not complete it should not be checked.', async ({ todoPage }) => {
     await todoPage.addTodo('Hello');
     await todoPage.addTodo('World');
     /** When nothing is checked */
@@ -22,16 +19,14 @@ test.describe('Toggle All', () => {
     await todoPage.itemCheckboxByIndex(0).click();
     await expect(todoPage.toggleAllCheckbox).not.toBeChecked();
   });
-  test('When all the todos are checked it should also get checked.', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('When all the todos are checked it should also get checked.', async ({ todoPage }) => {
     await todoPage.addTodo('Hello');
     await todoPage.addTodo('World');
     await todoPage.itemCheckboxByIndex(0).click();
     await todoPage.itemCheckboxByIndex(1).click();
     await expect(todoPage.toggleAllCheckbox).toBeChecked();
   });
-  test('This checkbox toggles all the todos to the same state as itself', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('This checkbox toggles all the todos to the same state as itself', async ({ todoPage }) => {
     await todoPage.addTodo('Hello');
     await todoPage.addTodo('World');
 
