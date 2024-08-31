5a68ae921103803de42d2e17746c6d82ddfdaeeb
basarat <basaratali@gmail.com>
2024-08-31 20:15:05 +1000

    better

--- solutions/todo/000.NoTodos.spec.ts
+++ solutions/todo/000.NoTodos.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from '../../tests/todo/test';
 
+test.beforeEach(async ({ clearTodosAndVisit }) => {
+  await clearTodosAndVisit();
+});
+
 test.describe('No Todos', () => {
   test('The main list and footer should be hidden', async ({ todoPage }) => {
     await expect(todoPage.main).not.toBeVisible();
--- tests/todo/test.ts
+++ tests/todo/test.ts
@@ -3,14 +3,21 @@ import { TodoPage } from './todoPage';
 
 export const test = base.extend<{
   todoPage: TodoPage;
+  clearTodosAndVisit: () => Promise<void>;
 }>({
   todoPage: async ({ page, request }, use) => {
     // setup (optional)
     const todoPage = new TodoPage(page);
-    await todoPage.clearTodosAndVisit(request);
     // use
     await use(todoPage);
     // teardown (optional)
+  },
+  clearTodosAndVisit: async ({ page, request }, use) => {
+    const clearTodosAndVisit = async () => {
+      const todoPage = new TodoPage(page);
+      await todoPage.clearTodosAndVisit(request);
+    };
+    await use(clearTodosAndVisit);
   },
 });
 
