837f48f06cf1b0393dc71249f9df0fdb16f29f1a
basarat <basaratali@gmail.com>
2024-08-31 20:16:09 +1000

    better

--- solutions/todo/060.ClearCompleted.spec.ts
+++ solutions/todo/060.ClearCompleted.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '../../tests/todo/test';
 import { TodoPage } from '../../tests/todo/todoPage';
 
 test.beforeEach(async ({ page, request }) => {
