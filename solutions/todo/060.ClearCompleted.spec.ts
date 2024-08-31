bee2785f384577744755531e95be907e6801253d
basarat <basaratali@gmail.com>
2024-08-31 20:20:15 +1000

    bettter

--- solutions/todo/060.ClearCompleted.spec.ts
+++ solutions/todo/060.ClearCompleted.spec.ts
@@ -1,25 +1,21 @@
 import { test, expect } from '../../tests/todo/test';
-import { TodoPage } from '../../tests/todo/todoPage';
 
 test.beforeEach(async ({ clearTodosAndVisit }) => {
   await clearTodosAndVisit();
 });
 
 test.describe('Clear Completed', () => {
-  test('Should be hidden when there are no completed todos', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('Should be hidden when there are no completed todos', async ({ todoPage }) => {
     await expect(todoPage.clearCompleted).not.toBeAttached();
     await todoPage.addTodo('Hello');
     await expect(todoPage.clearCompleted).not.toBeAttached();
   });
-  test('Should be visible when there are completed todos', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('Should be visible when there are completed todos', async ({ todoPage }) => {
     await todoPage.addTodo('Hello');
     await todoPage.itemCheckboxByIndex(0).click();
     await expect(todoPage.clearCompleted).toBeVisible();
   });
-  test('Clicking it removes completed todos', async ({ page }) => {
-    const todoPage = new TodoPage(page);
+  test('Clicking it removes completed todos', async ({ todoPage }) => {
     await todoPage.addTodo('Hello');
     await todoPage.itemCheckboxByIndex(0).click();
     await todoPage.clearCompleted.click();
