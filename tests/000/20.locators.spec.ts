28370e963d06ab71606b72921af1b1b507f4c7ee
basarat <basaratali@gmail.com>
2026-02-06 12:29:22 +1100

    updated

--- tests/000/20.locators.spec.ts
+++ tests/000/20.locators.spec.ts
@@ -1,17 +1,18 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
 /**
  * Note: using bing as google is blocked with "are you a robot"
  */
-test("Page demo", async ({ page }) => {
+test('Page demo', async ({ page }) => {
   // arrange
-  await page.goto("https://bing.com");
+  await page.goto('https://bing.com');
 
   // act
-  const input = page.locator("[type=Search]");
-  await input.fill("playwright");
-  await input.press("Enter");
+  const input = page.locator('[type=Search]');
+  await input.fill('playwright');
+  const updatedInput = page.locator('form[role=search]');
+  await updatedInput.press('Enter');
 
   // assert
-  await expect(page).toHaveTitle(/Search/);
+  await expect(page).toHaveTitle('playwright - Search');
 });
